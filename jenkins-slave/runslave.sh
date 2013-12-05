3592fa739d87a2c26c33240281f5dd15165e90e2
Wouter Danes <wouter.danes@gmail.com>
2013-12-05 14:53:49 +0100

    Fix to runslave.sh to make it more robust on restarts. It will only wget the slave.jar when needed and will now retry infinitely while the jenkins is starting up.

--- jenkins-slave/runslave.sh
+++ jenkins-slave/runslave.sh
@@ -5,5 +5,8 @@ JENKINS_AUTHORITY=${JENKINS_PORT_8080_TCP_ADDR}:${JENKINS_PORT_8080_TCP_PORT}
 SLAVE_JAR_URL="http://${JENKINS_AUTHORITY}/jnlpJars/slave.jar"
 JNLP_URL="http://${JENKINS_AUTHORITY}/computer/${SLAVE_NAME}/slave-agent.jnlp"
 
-wget ${SLAVE_JAR_URL} -O slave.jar
+if [ ! -f "slave.jar" ] ; then
+	wget --retry-connrefused -t inf ${SLAVE_JAR_URL} -O slave.jar
+fi
+
 java -jar slave.jar -jnlpUrl ${JNLP_URL}
